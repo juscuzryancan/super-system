4bcb45dbf3fcbfc74114b9113fe6de837baa9084
Ryan Riley Puzon <rrgpuzon@gmail.com>
2023-04-03 15:01:35 -0700

    displays if the client is connected or disconnected

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -6,38 +6,51 @@ import Stomp from "stomp-websocket";
 const Chat = () => {
   const [stompClient, setStompClient] = useState<any>(null);
   const [message, setMessage] = useState<string>("");
+  const [connected, setConnected] = useState(false);
   
-  const handleSocket = () => {
-    var socket = new SockJS('http://localhost:8080/gs-guide-websocket');
-    const stompClient = Stomp.over(socket)
-    setStompClient(stompClient);
-    // stompClient.connect({}, function (frame: any) {
-    //   console.log('Connected: ' + frame);
-    //   stompClient.subscribe('/topic/greetings', function (greeting: any) {
-    //     console.log(greeting);
-    //   });
-    // });
-  }
+
+//   function disconnect() {
+//     if (stompClient !== null) {
+//         stompClient.disconnect();
+//     }
+//     setConnected(false);
+//     console.log("Disconnected");
+// }
+
+// function sendName() {
+//     stompClient.send("/app/hello", {}, JSON.stringify({'name': $("#name").val()}));
+// }
+
+// function showGreeting(message) {
+//     $("#greetings").append("<tr><td>" + message + "</td></tr>");
+// }
+
 
   const handleConnect = () => {
-    console.log("handling connecting", stompClient);
-    stompClient.connect({}, function (frame: any) {
-      console.log('Connected: ' + frame);
-      stompClient.subscribe('/topic/greetings', function (greeting: any) {
-        console.log(greeting);
-      });
+    const socket = new SockJS('http://localhost:8080/gs-guide-websocket');
+    const stompClient = Stomp.over(socket);
+    setStompClient(stompClient);
+    stompClient.connect({}, function (frame:any) {
+        setConnected(true);
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/topic/greetings', function (greeting: any) {
+            console.log(JSON.parse(greeting.body).message);
+        });
     });
+  }
+
+  const handleDisconnect = () => {
+    if (stompClient !== null) {
+        stompClient.disconnect();
+    }
+    setConnected(false);
+    console.log("Disconnected");
   }
 
   const handleSend: FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
-    console.log("handling sending");
     stompClient.send("/app/hello", {}, JSON.stringify({'name': message}));
   }
-
-  useEffect(() => {
-    handleSocket();
-  }, []);
 
   return (
 
@@ -47,8 +60,15 @@ const Chat = () => {
         border-black border-2
         h-72 w-full" 
       />
+        {connected 
+        ? <div className="p-12 bg-green-700">Connected</div>
+        : <div className="p-12 bg-red-700">Disconnected</div>
+        }
       <button onClick={handleConnect}>
         Connect
+      </button>
+      <button onClick={handleDisconnect}>
+        Disconnect
       </button>
       <form onSubmit={handleSend}>
         <label>Message</label>
