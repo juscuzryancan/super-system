adab5b4d8a8308c1e66227b2d8c0afde118571d7
Ryan Riley Puzon <rrgpuzon@gmail.com>
2023-04-02 17:01:39 -0700

    connects to websocket in bckend

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, FormEventHandler } from 'react'
+import { useState, useEffect } from 'react'
 import { io, Socket } from "socket.io-client";
 import p5 from "p5";
 import Words from './components/Words';
@@ -6,42 +6,9 @@ import Chat from './components/Chat';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
+import SockJS from "sockjs-client";
 
 const App = () => {
-  const [socketId, setSocketId] = useState("");
-  const [message, setMessage] = useState("");
-  const [username, setUsername] = useState("");
-  // const [socket, setSocket] = useState(io("http://localhost:3000"));
-
-  // useEffect(() => {
-
-  //   socket.on("connect", () => {
-  //     console.log("this is my id", socket.id);
-  //     setSocketId(socket.id);
-  //   })
-
-  //   socket.on("receiveMessage", (data) => {
-  //     console.log("im receiving message: ", data);
-  //     const messagesElem = document.querySelector(".messages");
-  //     const newMessageElem = document.createElement("p");
-  //     newMessageElem.innerText = data;
-  //     if (messagesElem) {
-  //       messagesElem.append(newMessageElem);
-  //     }
-  //   });
-
-  //   return () => {
-  //     socket.off("connect");
-  //     socket.off("receiveMessage");
-  //   }
-
-  // }, [])
-
-  const handleSendMessage: FormEventHandler<HTMLFormElement> = (e) => {
-    e.preventDefault();
-    // socket.emit("message", message);
-    setMessage("");
-  }
 
   return (
     <div
@@ -57,6 +24,10 @@ const App = () => {
           <Route 
             path="/login"
             element={<div>hello</div>}
+          />
+          <Route
+            path="/start"
+            element={<Chat />}
           />
         </Routes>
       </main>
--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,18 +1,91 @@
+import { useState, useEffect, FormEventHandler } from "react";
 import Words from "./Words"
+// @ts-ignore
+import Stomp from "stomp-websocket";
+import SockJS from "sockjs-client";
+import { io } from "socket.io-client";
 
 const Chat = () => {
+  const [stompClient, setStompClient] = useState(null);
+  
+  const handleSocket = () => {
+    const sock = io("");
+
+    var socket = new SockJS('http://localhost:8080/gs-guide-websocket');
+    const stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame: any) {
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/topic/greetings', function (greeting: any) {
+            console.log(JSON.parse(greeting.body).content);
+        });
+    });}
+
+  useEffect(() => {
+    handleSocket();
+  }, []);
+
+//   function connect() {
+// 	ws = new WebSocket('ws://localhost:8080/name');
+// 	ws.onmessage = function(data){
+// 		showGreeting(data.data);
+// 	}
+// 	 setConnected(true);
+// }
+
+// function disconnect() {
+//     if (ws != null) {
+//         ws.close();
+//     }
+//     setConnected(false);
+//     console.log("Disconnected");
+// }
+
+// function sendName() {
+//     ws.send($("#name").val());
+// }
+
+// function showGreeting(message) {
+//     $("#greetings").append(" " + message + "");
+// }
+
+  const [socketId, setSocketId] = useState("");
+  const [message, setMessage] = useState("");
+  const [username, setUsername] = useState("");
+  // const [socket, setSocket] = useState(io("http://localhost:3000"));
+
+  // useEffect(() => {
+
+  //   socket.on("connect", () => {
+  //     console.log("this is my id", socket.id);
+  //     setSocketId(socket.id);
+  //   })
+
+  //   socket.on("receiveMessage", (data) => {
+  //     console.log("im receiving message: ", data);
+  //     const messagesElem = document.querySelector(".messages");
+  //     const newMessageElem = document.createElement("p");
+  //     newMessageElem.innerText = data;
+  //     if (messagesElem) {
+  //       messagesElem.append(newMessageElem);
+  //     }
+  //   });
+
+  //   return () => {
+  //     socket.off("connect");
+  //     socket.off("receiveMessage");
+  //   }
+
+  // }, [])
+
+  const handleSendMessage: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    // socket.emit("message", message);
+    setMessage("");
+  }
 
   return (
 
-    <div className="flex flex-col w-screen h-screen">
-      <header>
-        {/* Room Id: {socketId} */}
-      </header>
-
-      <div className="flex-grow">
-        <Words/>
-      </div>
-
+    <div className="flex flex-col">
       <div className="messages 
         overflow-auto
         border-black border-2
