68989663cb568e344b8166ed4f09546c477d8672
Ryan Riley Puzon <rrgpuzon@gmail.com>
2023-04-04 12:37:44 -0700

    recreates the subscription to take in a new reference of messages

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,50 +1,57 @@
-import { useState, useEffect, FormEventHandler } from "react";
+import { useState, useEffect, useCallback, FormEventHandler, MouseEventHandler } from "react";
 import SockJS from "sockjs-client";
 // @ts-ignore
 import Stomp from "stomp-websocket";
 
 const Chat = () => {
   const [stompClient, setStompClient] = useState<any>(null);
-  const [message, setMessage] = useState<string>("");
   const [connected, setConnected] = useState(false);
+  const [message, setMessage] = useState<string>("");
+  const [messages, setMessages] = useState<Array<any>>([]);
+
+  useEffect(() => {
+    if (!stompClient) {
+      return
+    }
+
+    stompClient.subscribe('/topic/greetings', function (greeting: any) {
+      const { body } = greeting;
+      const { message } = JSON.parse(body);
+      const newMessages = messages.map((message) => message);
+      newMessages.push({
+        content: message,
+        id: greeting.headers["message-id"]
+      });
+      setMessages(newMessages);
+    });
+  }, [messages])
   
-
-//   function disconnect() {
-//     if (stompClient !== null) {
-//         stompClient.disconnect();
-//     }
-//     setConnected(false);
-//     console.log("Disconnected");
-// }
-
-// function sendName() {
-//     stompClient.send("/app/hello", {}, JSON.stringify({'name': $("#name").val()}));
-// }
-
-// function showGreeting(message) {
-//     $("#greetings").append("<tr><td>" + message + "</td></tr>");
-// }
-
-
   const handleConnect = () => {
     const socket = new SockJS('http://localhost:8080/gs-guide-websocket');
     const stompClient = Stomp.over(socket);
     setStompClient(stompClient);
+
+    //frame comes out to be undefined
     stompClient.connect({}, function (frame:any) {
         setConnected(true);
-        console.log('Connected: ' + frame);
         stompClient.subscribe('/topic/greetings', function (greeting: any) {
-            console.log(JSON.parse(greeting.body).message);
+          const { body } = greeting;
+          const { message } = JSON.parse(body);
+          const newMessages = messages.map((message) => message);
+          newMessages.push({
+            content: message,
+            id: greeting.headers["message-id"]
+          });
+          setMessages(newMessages);
         });
     });
-  }
+  };
 
   const handleDisconnect = () => {
     if (stompClient !== null) {
         stompClient.disconnect();
     }
     setConnected(false);
-    console.log("Disconnected");
   }
 
   const handleSend: FormEventHandler<HTMLFormElement> = (e) => {
@@ -53,13 +60,20 @@ const Chat = () => {
   }
 
   return (
-
     <div className="flex flex-col">
       <div className="messages 
         overflow-auto
         border-black border-2
         h-72 w-full" 
-      />
+      >
+        {messages.map(({content, id}: any) => {
+          return (
+            <div key={id}>
+              {content}
+            </div>
+          )
+        })}
+      </div>
         {connected 
         ? <div className="p-12 bg-green-700">Connected</div>
         : <div className="p-12 bg-red-700">Disconnected</div>
@@ -70,10 +84,12 @@ const Chat = () => {
       <button onClick={handleDisconnect}>
         Disconnect
       </button>
-      <form onSubmit={handleSend}>
+      <form 
+        onSubmit={handleSend}
+      >
         <label>Message</label>
         <input className="border-black border-2 rounded" type="text" value={message} onChange={(e) => setMessage(e.target.value)}/>
-        <button type="submit">Send</button>
+        <button disabled={!connected} type="submit">Send</button>
       </form>
     </div>
   )
