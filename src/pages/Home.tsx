fb495c9f9697b53f11d8aa75a6f2e8cb46a7d0cd
Ryan Riley Puzon <rrgpuzon@gmail.com>
2023-05-17 13:45:42 -0700

    adds new footer section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ const Home = () => {
 
   return (
     <div
-      className="flex flex-col justify-center items-center gap-16
+      className="flex flex-col justify-center gap-16
       p-8"
     >
       <div 
@@ -23,14 +23,51 @@ const Home = () => {
         </Link>
       </div>
 
+      <section
+        className="flex flex-col items-center bg-secondary gap-4
+        text-white p-8 rounded"
+      >
+        <header className="text-3xl">Practice your Typing</header>
 
-      <section>
-        hello
       </section>
 
-      <section>
-        hello
+      <section
+        className="flex flex-col items-center bg-secondary gap-4
+        text-white p-8 rounded"
+      >
+        <header className="text-3xl">Multiplayer</header>
+
       </section>
+
+      <section
+        className="flex flex-col items-center bg-secondary gap-4
+        text-white p-8 rounded"
+      >
+        <header className="text-3xl">Track your statistics</header>
+      </section>
+
+      <footer className="flex flex-col bg-secondary gap-4
+        text-white p-8 rounded"
+      >
+        <header className="text-2xl">Mach Typer</header>
+        <div className="flex gap-4">
+          <div className="flex flex-col">
+            <header>
+              About
+            </header>
+            <Link to="/">somehwer</Link>
+          </div>
+          <div>
+            Use Cases
+          </div>
+          <div>
+            Company
+          </div>
+          <div>
+            Resources
+          </div>
+        </div>
+      </footer>
 
     </div>
   );
