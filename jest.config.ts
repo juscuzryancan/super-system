efd357e4772d1750a6ceaf5c332eda2922828762
Ryan Riley Puzon <rrgpuzon@gmail.com>
2023-09-20 18:29:36 -0700

    adds jest config

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+import * as path from "path";
+
+export default {
+  preset: "ts-jest",
+  testEnvironment: "jest-environment-jsdom",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+    // process `*.tsx` files with `ts-jest`
+  },
+  moduleNameMapper: {
+    "\\.(gif|ttf|eot|svg|png)$": "<rootDir>/test/__ mocks __/fileMock.js",
+  },
+  moduleDirectories: [
+    "node_modules",
+    path.join(__dirname, "test"),
+    path.join("src"),
+  ],
+};
