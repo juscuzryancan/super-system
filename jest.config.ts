dfaca4479c8f15383c57af5c90a5d095aacaca92
Ryan Riley Puzon <rrgpuzon@gmail.com>
2023-09-21 16:35:40 -0700

    formats config file

--- jest.config.ts
+++ jest.config.ts
@@ -1,18 +1,19 @@
-import * as path from "path";
+import * as path from 'path';
 
 export default {
-  preset: "ts-jest",
-  testEnvironment: "jest-environment-jsdom",
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
   transform: {
-    "^.+\\.tsx?$": "ts-jest",
+    '^.+\\.tsx?$': 'ts-jest',
     // process `*.tsx` files with `ts-jest`
   },
   moduleNameMapper: {
-    "\\.(gif|ttf|eot|svg|png)$": "<rootDir>/test/__ mocks __/fileMock.js",
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__ mocks __/fileMock.js',
   },
   moduleDirectories: [
-    "node_modules",
-    path.join(__dirname, "test"),
-    path.join("src"),
+    'node_modules',
+    __dirname,
+    path.join('test'),
+    path.join('src'),
   ],
 };
